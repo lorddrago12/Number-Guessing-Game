e0ecb4b6117040243e88e8f618239e1b26259dc4
Lord Drago <clearrio1465@gmail.com>
2025-12-17 19:54:02 +0530

    Update number guessing game.py

--- number guessing game.py
+++ number guessing game.py
@@ -1,18 +1,68 @@
 import random
 
-secret_number = random.randint(1, 100)
 player_guesses = 0
 
-while True:
-    player_guess = int(input("Enter Your Guess: "))
-    player_guesses = player_guesses + 1
+print("*****Number Guessing Game!*****")
+print("Choose Your Difficulty")
+print("1. Easy 1-  100")
+print("2. Medium 1 - 200")
+print("3. Hard 1 - 300")
 
-    if player_guess > secret_number:
-        print("Your Guess is too high, Try again!")
+player_difficulty = int(input("Enter Your difficulty: "))
+#choosing difficulty
+if player_difficulty == 1:
+    secret_number = random.randint(1, 100)
 
-    elif player_guess < secret_number:
-        print("Your guess is too low, Try again!")
+    while True:
 
-    else:
-        print(f"You guessed the correct number in {player_guesses} gusses.")
-        break
+        player_guess = int(input("Enter Your Guess: "))
+        player_guesses = player_guesses + 1
+
+        if player_guess > secret_number:
+            
+            print("Your Guess is too high, Try again!")
+
+        elif player_guess < secret_number:
+            print("Your guess is too low, Try again!")
+
+        else:
+            print(f"You guessed the correct number in {player_guesses} gusses.")
+            break
+
+elif player_difficulty == 2:
+    secret_number = random.randint(1, 200)
+        
+    while True:
+
+        player_guess = int(input("Enter Your Guess: "))
+        player_guesses = player_guesses + 1
+
+        if player_guess > secret_number:
+            
+            print("Your Guess is too high, Try again!")
+
+        elif player_guess < secret_number:
+            print("Your guess is too low, Try again!")
+
+        else:
+            print(f"You guessed the correct number in {player_guesses} gusses.")
+            break
+
+else:
+    secret_number = random.randint(1, 300)
+        
+    while True:
+
+        player_guess = int(input("Enter Your Guess: "))
+        player_guesses = player_guesses + 1
+
+        if player_guess > secret_number:
+            
+            print("Your Guess is too high, Try again!")
+
+        elif player_guess < secret_number:
+            print("Your guess is too low, Try again!")
+
+        else:
+            print(f"You guessed the correct number in {player_guesses} gusses.")
+            break
